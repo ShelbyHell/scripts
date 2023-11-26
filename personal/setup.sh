1c72014d981dd4f1740280c12d42e1d8099dfc23
Alexey Shelby <shelbyhell@proton.me>
2023-11-26 21:54:07 +0400

    scripts: personal: Import setup script.

---
+++ personal/setup.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+clear
+
+# Colours (or Colors in en_US)
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+PURPLE='\033[0;35m'
+BLUE='\033[0;34m'
+NORMAL='\033[0m'
+
+sudo apt update && sudo DEBIAN_FRONTEND=noninteractive \
+	apt install \
+	adb autoconf automake axel bc bison build-essential \
+	ccache clang cmake curl expat fastboot flex g++ \
+	g++-multilib gawk gcc gcc-multilib git git-lfs gnupg gperf \
+	htop imagemagick lib32ncurses5-dev lib32z1-dev libtinfo5 libc6-dev libcap-dev \
+	libexpat1-dev libgmp-dev '^liblz4-.*' '^liblzma.*' libmpc-dev libmpfr-dev libncurses5-dev \
+	libsdl1.2-dev libssl-dev libtool libxml2 libxml2-utils '^lzma.*' lzop \
+	maven ncftp ncurses-dev patch patchelf pkg-config pngcrush \
+	pngquant python2.7 python-all-dev re2c schedtool squashfs-tools subversion \
+	texinfo unzip w3m xsltproc zip zlib1g-dev lzip \
+	libxml-simple-perl libswitch-perl apt-utils libncurses5 curl python-is-python3 byobu rclone llvm -y || abort "Setup Failed!"
+
+echo "Installing repo"
+sudo curl --create-dirs -L -o /usr/local/bin/repo -O -L https://storage.googleapis.com/git-repo-downloads/repo
+sudo chmod a+rx /usr/local/bin/repo
+
+echo "rclone install"
+sudo -v ; curl https://rclone.org/install.sh | sudo bash
+
+sudo fallocate -l 8G /swapfile
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+
+git config --global user.email "shelbyhell@proton.me"
+git config --global user.name "Alexey Shelby"
+git config --global review.gerrit.aospa.co.username "ShelbyHell"
+
+git config --global alias.cp 'cherry-pick'
+git config --global alias.c 'commit'
+git config --global alias.f 'fetch'
+git config --global alias.m 'merge'
+git config --global alias.rb 'rebase'
+git config --global alias.rs 'reset'
+git config --global alias.ck 'checkout'
+git config --global credential.helper 'cache --timeout=99999999'
+
+echo -e "\nSetting up shell environment..."
+if [[ $SHELL = *zsh* ]]; then
+sh_rc=".zshrc"
+else
+sh_rc=".bashrc"
+fi
+
+cat <<'EOF' >> $sh_rc
+
+# Super-fast repo sync
+repofastsync() { time schedtool -B -e ionice -n 0 `which repo` sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --retry-fetches=5 -j8 "$@"; }
+
+aospainit() { time schedtool -B -e ionice -n 0 `which repo` init --depth=1 --no-repo-verify -u https://github.com/AOSPA/manifest -b uvite -g default,-mips,-darwin,-notdefault
+ "$@"; }
+
+repofailfetch() { time schedtool -B -e ionice -n 0 `which repo` sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --retry-fetches=5 -j8 "$@"; }
+
+export TZ='Europe/Samara'
+export USE_CCACHE=1
+export CCACHE_EXEC=/usr/bin/ccache
+
+EOF
+
+# Unlimited history file
+sed -i 's/HISTSIZE=.*/HISTSIZE=-1/g' $sh_rc
+sed -i 's/HISTFILESIZE=.*/HISTFILESIZE=-1/g' $sh_rc
+
+echo -e "Done."
+
+# Increase tmux scrollback buffer size
+echo "set-option -g history-limit 6000" >> .tmux.conf
+
+echo -e ${GREEN}"Setup Complete!"${NORMAL}
